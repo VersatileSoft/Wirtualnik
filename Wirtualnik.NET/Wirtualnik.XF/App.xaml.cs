54b945d2c04040f2eb014b92a77f3e00b84c5bee
Kacper Tryniecki <kacper@tryniecki.com>
2021-06-18 18:51:19 +0200

    #5 Reduce sending logs on debug configuration

--- Wirtualnik.NET/Wirtualnik.XF/App.xaml.cs
+++ Wirtualnik.NET/Wirtualnik.XF/App.xaml.cs
@@ -21,8 +21,11 @@ namespace Wirtualnik.XF
                 options.Dsn = "https://6d41b18b43a74862b64c0239985b9ee8@o866902.ingest.sentry.io/5823541";
 #if DEBUG
                 options.Debug = true;
+                options.TracesSampleRate = 0;
 #endif
+#if RELEASE
                 options.TracesSampleRate = 1.0;
+#endif
                 options.AddXamarinFormsIntegration();
             });
 
