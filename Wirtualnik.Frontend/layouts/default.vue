870a2dbf7da2078285ad182d2dfc7ae764b61617
k.kolasa <k.kolasa@ideo.pl>
2021-06-29 17:36:11 +0200

    create sitemap and breadcrumb component

--- Wirtualnik.Frontend/layouts/default.vue
+++ Wirtualnik.Frontend/layouts/default.vue
@@ -2,6 +2,7 @@
   <div class="app container">
     <Header />
     <div class="page-content">
+      <BreadCrumb />
       <nuxt />
     </div>
   </div>
@@ -10,21 +11,23 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import Header from '@/components/common/Header.vue'
+import BreadCrumb from '@/components/navigation/Breadcrumb.vue'
 
 @Component({
   name: 'DefaultLayout',
   components: {
     Header,
+    BreadCrumb,
   },
 })
 export default class DefaultLayout extends Vue {}
 </script>
 
 <style lang="scss" scoped>
-  .page-content {
-    padding-top: 65px;
-    @include for-tablet-landscape-up {
-      padding-top: 0;
-    }
+.page-content {
+  padding: 65px 10px 0 10px;
+  @include for-tablet-landscape-up {
+    padding-top: 0;
   }
+}
 </style>
