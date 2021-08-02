780225a37df4f153f553db4689dd705563effe26
ErgoAsh <wiedzmin1371@gmail.com>
2021-08-02 14:17:21 +0200

    Create non-optional route for product page

---
+++ Wirtualnik.Frontend/pages/p/_id/index.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+@Component({
+  name: 'ProductsHorizontalTrack',
+})
+export default class ProductPage extends Vue {
+
+    public get id() {
+        return this.$route.params.id;
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
