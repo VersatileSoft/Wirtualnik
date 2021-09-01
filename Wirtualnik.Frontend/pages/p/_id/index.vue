f910f6bab45f92ee0680acf4e467503fe4f75f54
Tomasz Kowalski <tomkowa64@gmail.com>
2021-09-01 11:23:28 +0200

    Temporary product page changes

--- Wirtualnik.Frontend/pages/p/_id/index.vue
+++ Wirtualnik.Frontend/pages/p/_id/index.vue
@@ -1,11 +1,10 @@
 <template>
     <div>
         <div class="head">
-            <div class="carousel"></div>
-
+            <ImageCarousel />
             <div class="details">
-                <h2>{{ this.item.data.name }}</h2>
-                <p>{{ this.item.data.description }}</p>
+                <h2>{{ item.name }}</h2>
+                <p>{{ item.description }}</p>
                 <button>Dodaj to koszyka</button>
             </div>
 
@@ -33,10 +32,7 @@
 
         <div class="spec">
             <h2>Specyfikacja</h2>
-            <ul
-                v-for="property in this.item.data.properties"
-                :key="property.publicId"
-            >
+            <ul v-for="property in item.properties" :key="item.publicId">
                 <p>{{ property.key }}</p>
                 <p>{{ property.value }}</p>
             </ul>
@@ -47,28 +43,81 @@
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator';
 import axios from 'axios';
+import BreadCrumb from '@/components/navigation/Breadcrumb.vue';
+import ImageCarousel from '@/components/common/ImageCarousel.vue';
 
 @Component({
-    name: 'ProductPage'
+    name: 'ProductPage',
+    components: {
+        BreadCrumb,
+        ImageCarousel
+    }
 })
 export default class ProductPage extends Vue {
+    private item: any[] = [];
+
     public get id() {
         return this.$route.params.id;
     }
 
     public async created(): Promise<void> {
+        await this.loadData();
+
+        this.$store.commit('breadcrumb/SET_BREADCRUMBS', [
+            {
+                name: 'Wirtualnik.pl',
+                route: '/'
+            },
+            {
+                name: this.item.productTypeName,
+                route: '/c/' + this.item.productTypeName
+            },
+            {
+                name: this.item.name,
+                route: '/p/' + this.item.publicId
+            }
+        ]);
+    }
+
+    private async loadData(): Promise<boolean> {
         try {
-            this.item = await axios.get(
+            const response = await axios.get(
                 'https://api.zlcn.pro/api/product/' + this.id
             );
-            console.log(this.item);
+            console.log(response);
+            this.item = response.data;
         } catch (ex) {
-            console.log(ex);
-            console.log(this.id);
             this.item = undefined;
         }
     }
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+:root {
+    --background: #f3f3f3;
+}
+body {
+    background-color: var(--background);
+    font-family: 'Poppins Devanagari', -apple-system, BlinkMacSystemFont,
+        'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji',
+        'Segoe UI Emoji', 'Segoe UI Symbol';
+    min-width: 320px;
+    font-size: 15px;
+    max-width: 1280px;
+    margin: 0 auto;
+}
+a {
+    text-decoration: none;
+}
+@keyframes fadeIn {
+    from {
+        opacity: 0;
+        transform: translate3d(0, 10px, 0);
+    }
+    to {
+        opacity: 1;
+        transform: translate3d(0, 0, 0);
+    }
+}
+</style>
