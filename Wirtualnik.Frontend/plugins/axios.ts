216793722706c2dbdb0e142a9f63c00029a23e61
Michał Woźniak <m.wozniak@ideo.pl>
2021-09-22 21:42:18 +0200

    Test log

--- Wirtualnik.Frontend/plugins/axios.ts
+++ Wirtualnik.Frontend/plugins/axios.ts
@@ -2,7 +2,9 @@ import Vue from 'vue';
 import ProductService from '../services/ProductService';
 import CartService from '../services/CartService';
 
-export default function ({ $axios }: never): void {
+export default function ({ $axios }: any): void {
+    console.log('From plugin');
+    console.log($axios.defaults.baseURL);
     Vue.prototype.$productService = new ProductService($axios);
     Vue.prototype.$cartService = new CartService($axios);
 }
